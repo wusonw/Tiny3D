b2514bb55af929d093d8003c349981d048fc6a2f
wangwuzhen <wangwuzhen@nuaa.edu.cn>
2022-10-19 03:29:42 +0800

    test: change math_test & add tiny3d_test

--- src/__test__/module/math.test.ts
+++ src/__test__/module/math.test.ts
@@ -1,17 +1,17 @@
-import { transpose, inverse } from "../../module/math";
+import { m4 } from "../../module/math";
 
 test("矩阵转置 transpose", () => {
-  const r = transpose([
-    1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-  ]).toString();
+  const r = m4
+    .transpose([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
+    .toString();
   const t = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1].toString();
   expect(r).toBe(t);
 });
 
 test("矩阵取逆 inverse", () => {
-  const r = inverse([
-    1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-  ]).toString();
+  const r = m4
+    .inverse([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
+    .toString();
   const t = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1].toString();
   expect(r).toBe(t);
 });
---
+++ src/__test__/module/tiny3d.test.ts
@@ -0,0 +1,5 @@
+import { Tiny3D } from "src/module/tiny3d";
+
+const tiny = new Tiny3D(document.createElement("canvas"));
+
+test("tiny3d.ts", () => {});
