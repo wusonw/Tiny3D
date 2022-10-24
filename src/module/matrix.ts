202f53e8ad0fa5a22b86a393199fee178814973c
wangwuzhen <wangwuzhen@nuaa.edu.cn>
2022-10-24 22:47:34 +0800

    chore: add math.ts

--- src/module/matrix.ts
+++ src/module/matrix.ts
@@ -1,5 +1,6 @@
 import { Camera, OrthoCamera, PerspectiveCamera } from "./camera";
 import { Geometry } from "./geometry";
+import { cross, dot, dToRad, normalize, subtract } from "./math";
 import { OrthoCameraViewport, PerspectiveCameraViewport, Vector } from "./type";
 const { tan, sin, cos, PI } = Math;
 
@@ -154,31 +155,3 @@ const S = (sx: number, sy: number, sz: number) => [
   0,
   1,
 ];
-
-const dToRad = (angle: number) => (angle * PI) / 180;
-
-const cross = (vec1: Vector, vec2: Vector): Vector => [
-  vec1[1] * vec2[2] - vec2[1] * vec1[2],
-  vec2[0] * vec1[2] - vec1[0] * vec2[2],
-  vec1[0] * vec2[1] - vec2[0] * vec1[1],
-];
-
-const subtract = (vec1: Vector, vec2: Vector): Vector => [
-  vec1[0] - vec2[0],
-  vec1[1] - vec2[1],
-  vec1[2] - vec2[2],
-];
-
-const dot = (vec1: Vector, vec2: Vector): number => {
-  return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2];
-};
-
-const normalize = (v: Vector): Vector => {
-  const length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-  // 确定不会除以 0
-  if (length > 0.00001) {
-    return [v[0] / length, v[1] / length, v[2] / length];
-  } else {
-    return [0, 0, 0];
-  }
-};
